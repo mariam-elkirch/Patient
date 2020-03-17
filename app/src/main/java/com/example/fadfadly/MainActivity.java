3a6a3c757fc193ea33a5029ec994e3ce9448e232
Mariam Elkirch <45265655+mariam-elkirch@users.noreply.github.com>
2020-03-17 23:30:41 +0200

    fadfadly

---
+++ app/src/main/java/com/example/fadfadly/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.fadfadly;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FirebaseFirestore firebaseFirestore=FirebaseFirestore.getInstance();
+        setContentView(R.layout.activity_main);
+    }
+}
